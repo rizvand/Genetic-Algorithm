0997a961d276e993b84e2bbe43eb5cbadc8be519
rizvan <rizvan@gmail.com>
2021-04-04 11:04:44 +0700

    add simulation report

--- GA.py
+++ GA.py
@@ -1,192 +1,166 @@
 import numpy as np
+from numpy.random import default_rng
 from testfunction import ackley, sphere
+import copy
 
-class Chromosome():
-    def __init__(self, values, random_state=1):
+class Chromosome:
+    def __init__(self, values, boundary):
         self.values = np.array(values)
+        self.boundary = boundary
         self.dim = len(values)
-        np.random.seed(random_state)
     
-    def fit(self, f_obj):
-        self.fitness = f_obj(self.values)
+    def fit(self, fitness_function):
+        self.fitness = fitness_function(self.values)
     
-    def mutate(self, method='swap', mutation_prob=0.5):
+    def mutation(self, method, mutation_prob, random_state):
+        generator = default_rng(seed=random_state)
         if method == 'swap':
-            idx1, idx2 = np.random.randint(self.dim, size=2)
+            idx1, idx2 = generator.choice(self.dim, size=2)
             self.values[[idx1, idx2]] = self.values[[idx2, idx1]]
     
-    def crossover(self, maternal_chromosome, method='one-point'):
-        if method == 'one-point':
-            point = int(np.random.randint(self.dim, size = 1))
-            self.values[:point] = maternal_chromosome.values[:point].copy()
-            
-        elif method == 'two-point':
-            r1, r2 = np.random.randint(self.dim, size = 2)
-            point1 = min(r1,r2)
-            point2 = max(r1,r2)
-            
-            self.values[:point1] = maternal_chromosome.values[:point1].copy()
-            self.values[point2:] = maternal_chromosome.values[point2:].copy()
-        
-        #fix this
-        elif method == 'uniform':
-            r = np.random.randint(2, size = self.dim)
-            for i in range(self.dim):
-                if r[i] == 1:
-                    self.values[i] = maternal_chromosome.values[i].copy()
-        return self
-    
-class Population():
-    def __init__(self, random_state=1):
-        self.best_chromosome = None
-        self.best_fitness = None
-        np.random.seed(random_state)
-        
-    def initialize(self, size, boundary):
-        population = []
-        for i in range(size):
-            values = []
-            for b in boundary:
-                if b[2] == 'int':
-                    val = np.random.randint(b[0], b[1]+1)
-                elif b[2] == 'float':
-                    val = np.random.uniform(b[0], b[1]) 
-                values.append(val)
-            initial_chromosome = Chromosome(values, random_state = i)
-            population.append(initial_chromosome)
-        self.history_population = population
-        self.pop_size = size
-    
-    def get_fitness(self, f_obj):
-        self.history_fitness = []
-        for x in self.history_population:
-            x.fit(f_obj)
-            self.history_fitness.append(x.fitness)
-    
-    def get_best(self, objective='minimum'):
-        if objective == 'minimum':
-            idx_best = np.argmin(self.history_fitness)
-        elif objective == 'maximum':
-            idx_best = np.argmax(self.history_fitness)
-        self.best_fitness = self.history_fitness[idx_best]
-        self.best_chromosome = self.history_population[idx_best]
-    
-    def selection(self, method):
-        if method == 'roulette':
-            #return selection proba for each chromosome (list)
-            self.selection_method = 'roulette'
-            sum_of_fitness = sum(self.history_fitness)
-            try:
-                if sum_of_fitness != 0:
-                    self.roulette_proba = np.array(self.history_fitness)/(sum_of_fitness)
-                else:
-                    print('masuk')
-                    self.roulette_proba = np.ones(self.pop_size)/self.pop_size
-            except:
-                self.roulette_proba = np.ones(self.pop_size)/self.pop_size
-                
-        elif method == 'tournament':
-            #fixed tournament size = half of total population
-            self.selection_method == 'tournament'
-            pairs = []
-            tournament_size = round(self.pop_size/4)
-            for i in range(pop_size):
-                pool_idx = np.random.randint(0, len(self.history_population), size=tournament_size)
-                pool_fitness = np.array(self.history_fitness)[[pool_idx]]
-                best_two = pool_fitness.argsort()[:2]
-                idx = pool_idx[best_two]
-                couple = self.history_population[idx[0]], self.history_population[idx[1]]
-                pairs.append(couple)
-            
-    def crossover(self, method):
-        if self.selection_method == 'roulette':
-            self.offspring_population = []
-            cdf_proba = np.array([self.roulette_proba[:i+1].sum() for i in range(self.roulette_proba.shape[0])])
-            for i in range(self.pop_size):
-                r1 = np.random.rand()
-                r2 = np.random.rand()
-                
-                idx1 = np.min(np.where(r1 <= cdf_proba))
-                idx2 = np.min(np.where(r2 <= cdf_proba))
-                    
-                paternal = self.history_population[idx1]
-                maternal = self.history_population[idx2]
-                offspring = paternal.crossover(maternal, method=method)
-                self.offspring_population.append(offspring)
-                
-        elif self.selection_method == 'tournament':
-            self.offspring_population = []
-            for pair in pairs:
-                paternal = pair[0]
-                maternal = pari[1]
-                offspring = paternal.crossover(maternal, method=method)
-                self.offspring_population.append(offspring)
-              
-    def mutation(self, method, prob, boundary):
-        def cut(x, bound):
-            if x < bound[0]:
-                x = bound[0]
-            elif x > bound[1]:
-                x = bound[1]
-            if bound[2] == 'float':
-                x = float(x)
-            elif bound[2] == 'int':
-                x = int(x)
-            return x
-        
-        for offspring in self.offspring_population:
-            offspring.mutate(method=method, mutation_prob=prob)
-            for i, bound in enumerate(boundary):
-                cut(offspring.values[i], bound)
-    
-    def replacement(self, method, frac, obj_func):
-        if method == 'partial':
-            num_keep = int(frac*self.pop_size)
-            offspring_fitness = np.array([obj_func(x.values) for x in self.offspring_population])
-            idx_offspring_keep = offspring_fitness.argsort()[:num_keep]
-            offspring_keep = np.array(self.offspring_population)[idx_offspring_keep]
-            idx_keep = np.array(self.history_fitness).argsort()[:num_keep]
-            pop_keep = np.array(self.history_population)[idx_keep]
-            self.history_population = np.append(pop_keep, offspring_keep).tolist()
+    def penalty(self):
+        for i in range(self.dim):
+            if self.values[i] > self.boundary[i][1]:
+                self.values[i] == self.boundary[i][0]
+            elif self.values[i] < self.boundary[i][0]:
+                self.values[i] == self.boundary[i][0]
 
 class GeneticAlgorithm:
-    def __init__(self, size, selection, crossover, mutation, replacement, crossover_prob, mutation_prob, replacement_frac, n_iter, boundary, objective_function, objective, random_state, save_history):
+    def __init__(self, size, n_iter, fitness_function, boundary, selection, crossover, mutation, elitism, crossover_prob, mutation_prob, save_history, random_state):
         self.size = size
         self.n_iter = n_iter
+        self.fitness_function = fitness_function
         self.boundary = boundary
-        self.objective_function = objective_function
-        self.objective = objective
         self.selection_method = selection
         self.crossover_method = crossover
         self.mutation_method = mutation
-        self.replacement_method = replacement
+        self.elitism = elitism
         self.crossover_prob = crossover_prob
         self.mutation_prob = mutation_prob
-        self.replacement_frac = replacement_frac
-        self.random_state = random_state
         self.save_history = save_history
-    
-    def simulate(self):
-        population = Population(random_state = self.random_state)
-        population.initialize(self.size, self.boundary)
-        population.get_fitness(self.objective_function)
-        population.get_best(objective='minimum')
+        self.rng = default_rng(seed=random_state)
+        self.dim = len(boundary)
+        np.random.seed(random_state)
         
-        if self.save_history:
-            self.history = []
-            self.history.append(population.history_population)
+    def roulette_selection(self, population):
+        population_fitness = np.array([x.fitness for x in population])
+        sum_of_fitness = population_fitness.sum()
+        if sum_of_fitness != 0:
+            roulette_proba = (sum_of_fitness-population_fitness)/(sum_of_fitness)
+            cdf_proba = np.array([roulette_proba[:i+1].sum() for i in range(roulette_proba.shape[0])])
+            r1 = np.random.rand()
+            r2 = np.random.rand()
+            idx1 = np.min(np.where(r1 <= cdf_proba))
+            idx2 = np.min(np.where(r2 <= cdf_proba))
+            paternal = copy.deepcopy(population[idx1])
+            maternal = copy.deepcopy(population[idx2])
+        else:
+            paternal = copy.deepcopy(population[0])
+            maternal = copy.deepcopy(population[1])
+        return paternal, maternal
+
+    def tournament_selection(self, population):
+        tournament_size = round(self.size/4)
+        tournament_idx = self.rng.choice(self.size, size=tournament_size, replace=False)
+        tournament_pool = copy.deepcopy(np.array(population)[tournament_idx])
+        tournament_fitness = [x.fitness for x in tournament_pool]
+        best_idx = np.array(tournament_fitness).argsort()[:2]
+        paternal, maternal = copy.deepcopy(tournament_pool[best_idx[0]]), copy.deepcopy(tournament_pool[best_idx[1]]) 
+        return paternal, maternal
+
+    def pair_selection(self, population):
+        if self.selection_method == 'roulette':
+            paternal, maternal = self.roulette_selection(population)
             
-        iteration = 0
-        print('iteration: ', iteration, ' | best chromosome: ', population.best_chromosome.values, ' | best fitness: ', population.best_fitness)
+        elif self.selection_method == 'tournament':
+            paternal, maternal = self.tournament_selection(population)
+            
+        return paternal, maternal
+
+    def crossover(self, paternal_chromosome, maternal_chromosome):
+        if self.crossover_method == 'one-point':
+            random_point = np.random.randint(self.dim)
+            offspring = copy.deepcopy(paternal_chromosome)
+            offspring.values[random_point:] = copy.deepcopy(maternal_chromosome.values[random_point:])
+            return offspring
+
+        elif self.crossover_method == 'two-point':
+            random_point = np.sort(self.rng.choice(self.dim, size = 2))
+            offspring = copy.deepcopy(paternal_chromosome)
+            offspring.values[random_point[0]:random_point[1]] = copy.deepcopy(maternal_chromosome.values[random_point[0]:random_point[1]])
+            return offspring
         
+        elif self.crossover_method == 'uniform':
+            offspring = copy.deepcopy(paternal_chromosome)
+            for i in range(self.dim):
+                r = np.random.rand()
+                if r > self.crossover_prob:
+                    offspring.values[i] = copy.deepcopy(maternal_chromosome.values[i])
+            return offspring
+
+    def simulate(self):
+        # Initialize
+        population = []
+        for i in range(self.size):
+            position = []
+            for pos_bound in self.boundary:
+                pos_ = np.random.uniform(pos_bound[0], pos_bound[1])
+                position.append(pos_)
+            initial_chromosome = Chromosome(position, self.boundary)
+            initial_chromosome.fit(self.fitness_function)
+            population.append(initial_chromosome)
+
+        iteration_fitness = np.array([x.fitness for x in population])
+        best_idx = np.argmin(iteration_fitness)
+        iteration_best = population[best_idx].values, population[best_idx].fitness
+
+        if self.save_history == True:
+            self.population_history = []
+            self.fitness_history = []
+            self.best_history = []
+            self.population_history.append([x.values for x in population.copy()])
+            self.fitness_history.append(iteration_fitness.copy())
+            self.best_history.append(iteration_best)
+        
+        iteration = 0
+#         print(f'iteration: {iteration} | best_position : {iteration_best[0]} | best_fit : {iteration_best[1]}')
+
+        #Optimization
         while iteration < self.n_iter:
             iteration += 1
-            population.selection(method=self.selection_method)
-            population.crossover(method=self.crossover_method)
-            population.mutation(method=self.mutation_method, prob=self.mutation_prob, boundary=self.boundary)
-            population.replacement(method=self.replacement_method, frac=self.replacement_frac, obj_func=self.objective_function)
-            population.get_fitness(self.objective_function)
-            population.get_best(objective='minimum')
-            if self.save_history:
-                self.history.append(population.history_population)
-            print('iteration: ', iteration, ' | best chromosome: ', population.best_chromosome.values, ' | best fitness: ', population.best_fitness)
+            offspring_population = []
+            for j in range(self.size):
+                paternal, maternal = self.pair_selection(population)
+                offspring = self.crossover(paternal, maternal)
+                offspring.mutation(method=self.mutation_method, mutation_prob=self.mutation_prob, random_state=j*iteration)
+                offspring.penalty()
+                offspring.fit(self.fitness_function)
+                offspring_population.append(offspring)
+
+            # get (1 - elitism) offspring chromosome
+            offspring_fitness = np.array([x.fitness for x in offspring_population])
+            num_keep = int((1 - self.elitism)*self.size)
+            keep_idx = offspring_fitness.argsort()[:num_keep]
+            selected_offspring = np.array(offspring_population)[keep_idx]
+
+            # get elitism previous generation chromosome
+            population_fitness = np.array([x.fitness for x in population])
+            num_keep = int(self.elitism*self.size)
+            keep_idx = population_fitness.argsort()[:num_keep]
+            selected_population = np.array(population)[keep_idx]
+
+            new_population = np.concatenate((selected_population, selected_offspring))
+            iteration_fitness = np.array([x.fitness for x in new_population])
+            best_idx = np.argmin(iteration_fitness)
+            iteration_best = new_population[best_idx].values, new_population[best_idx].fitness
+#             print(f'iteration: {iteration} | best_position : {iteration_best[0]} | best_fit : {iteration_best[1]}')
+            population = new_population.copy()
+
+            if self.save_history == True:
+                self.population_history.append([x.values for x in population])
+                self.fitness_history.append(iteration_fitness)
+                self.best_history.append(iteration_best)
+
+            
+        
+
